8dbd9660a7e412928f04f2a7824bc5cf017658f8
Oleksandr Tympshenko <hermeusmanua@gmail.com>
2023-07-16 16:24:11 +0300

    remmoved useEffect import from App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,69 +4,7 @@ import { ContactList } from './ContactList';
 import { Filter } from './Filter';
 import { nanoid } from 'nanoid';
 import { save, load } from 'js/storage';
-import { useState, useEffect } from 'react';
-// import React, { Component } from 'react';
-
-// export class App extends Component {
-//   state = {
-//     filter: '',
-//   }
-
-//   constructor(props) {
-//     super(props);
-
-//     this.state.contacts = (typeof load("contacts") === "object" &&
-//       load("contacts") !== null) ?
-//       load("contacts") : [];
-//   }
-
-//   onSubmit = (evt) => {
-//     evt.preventDefault();
-    // if (this.state.contacts.filter(el => el.name === this.state.name).length > 0) {
-    //     window.alert(`${this.state.name} is already in contacts`)
-    // } else {
-    //    this.setState((state) => {
-    //     save("contacts", [...state.contacts, { name: state.name, number: state.number, id: nanoid(), }]);
-        
-    //     return {
-    //       contacts: [...state.contacts, { name: state.name, number: state.number, id: nanoid(), }],
-    //       name: '',
-    //       number: '',
-    //       filter: ''
-    //     }
-    //     })
-    // }
-//   }
-
-//   onInput = (evt) => {
-//     if (evt.target.getAttribute("name") === "name") {
-//       this.setState({ name: evt.target.value });
-//     } else if ((evt.target.getAttribute("name") === "number")) {
-//       this.setState({ number: evt.target.value });
-//     } else {
-//       this.setState({ filter: evt.target.value });
-//     }
-//   }
-  
-//   onDelete = (evt) => {
-    // let deleteIndex;
-    // this.state.contacts.forEach((el, i) => {
-    //   if (evt.target.previousElementSibling.textContent.includes(el.name)) {
-    //       deleteIndex = i;
-    //   }
-    // })
-    // this.setState((state) => {
-    //   let counter = state.contacts.filter((el, i) => i !== deleteIndex);
-    //   save("contacts", counter);
-    //   return { contacts: counter };
-    // })
-//   }
-
-//   render() {
-//     const { contacts, name, number, filter } = this.state;
-
-//   }
-// };
+import { useState } from 'react';
 
 export const App = () => { 
   const [filter, setFilter] = useState(''); 
